effb715087f38359ccd84661d65c439ae870df7b
Facundo Garcia <facundogg7@gmail.com>
2021-07-20 14:06:39 -0300

    media

--- madeInChaco/settings.py
+++ madeInChaco/settings.py
@@ -142,7 +142,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'media')
 ]
 
 # Default primary key field type
